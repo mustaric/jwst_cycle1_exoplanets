55ca87e569c6161cae436016d76ef3e68aff88c8
Susan Mullally <smullally@stsci.edu>
2022-05-18 15:59:08 -0400

    Updated TESS planet plots and put in incident flux

--- cycle1_exoplanets_tess.py
+++ cycle1_exoplanets_tess.py
@@ -3,28 +3,21 @@
 """
 Created on Wed May 19 09:02:29 2021
 
-@author: 
-    
-Plots updated to include which ones were discoverd by TESS that
-will be observed in Cycle 1 JWST.
+@author: smullally
 """
-
-
 
 import numpy as np
 import requests
 import matplotlib.pyplot as plt
-import pandas as p
+import pandas as pd
 from IPython.display import display, HTML
 import re
 
-
+planeturl = "https://exo.mast.stsci.edu/api/v0.1/exoplanets/"
+dvurl = "https://exo.mast.stsci.edu/api/v0.1/dvdata/tess/"
+header={}
 
 def get_planet_properties(planet_name):
-
-    planeturl = "https://exo.mast.stsci.edu/api/v0.1/exoplanets/"
-    dvurl = "https://exo.mast.stsci.edu/api/v0.1/dvdata/tess/"
-    header={}
     
     if (planet_name[0:3] == 'TOI') & (re.match("b|c|d|e|f|g|h|j", planet_name[-1]) is not None):
         planet_name = planet_name.replace(' ', '-', 1)
@@ -37,16 +30,18 @@ def get_planet_properties(planet_name):
 
     planet_prop = r.json()
     
-    #print(len(planet_prop))
+    print(len(planet_prop))
     if len(planet_prop) == 2:
-        return planet_prop[1]['Rp'], planet_prop[1]['Tp']
+        return planet_prop[1]
     elif len(planet_prop) == 1:
-        return planet_prop[0]['Rp'], planet_prop[0]['Tp']
+        return planet_prop[0]
     else:
-        return -1, -1
+        return {'planet_name' : planet_name, 'catalog_name' : 'not found'}
 #%%
-#Read in which objects will be 
-ddir = "/Users/smullally/Python_Code/jwst_project/cycle1_science/"
+#Create two dictionaries
+# one for the jwst planets
+# one for the planets previously observed by HST/Spitzer
+ddir = "/Users/smullally/Python_Code/jwst_project/cycle1_science/jwst_cycle1_exoplanets/"
 jwst_files = ['cycle1_planets_transit.txt',
              'cycle1_planets_direct.txt',
              'gto_planets_transit.txt',
@@ -61,79 +56,123 @@ all_dfs = []
 
 for i,f in enumerate(jwst_files):
     
-    df = p.read_csv(ddir+f, delimiter=",",names=['planet', 'inst'])
+    df = pd.read_csv(ddir+f, delimiter=",",names=['planet', 'inst'])
     df['calltype'] =jwst_types[i]
     df['obstype'] = obs_types[i]
     
     all_dfs.append(df)
 
-jwstdf = p.concat(all_dfs,axis=0)
+jwstdf = pd.concat(all_dfs,axis=0)
 
-previousdf = p.read_csv(ddir+previous_file, comment="#")
+previousdf = pd.read_csv(ddir+previous_file, comment="#")
 
 unique_prev = previousdf[['plntname', 'instrument']].drop_duplicates()
 
 #%%
+#Gather information about the JWST targets
+params = ['Rp', 'Tp', 'Mp','Teff', 'Kmag', 'orbital_period', 'Rs','a/Rs', 'catalog_name', 'planet_name']
 
-Rp = []
-Tp = []
+planetdf  = pd.DataFrame(columns = params)
 
 for i,planet in jwstdf.iterrows():
     
     try:
-        radii, temp = get_planet_properties(planet['planet'])
-        #print(radii, temp)
+        planetdict = get_planet_properties(planet['planet'])
+        shortdf = pd.DataFrame([planetdict], columns = params)
+        print(shortdf)
     except:
-        radii = -1
-        temp = -1
-        
-    Rp.append(radii)
-    Tp.append(temp)
+        shortdf = pd.DataFrame([{'planet_name' : planet['planet'], 'catalog_name' : 'error'}])
+    
+    planetdf = pd.concat([planetdf, shortdf])    
+    
+final_jwstdf = pd.merge(jwstdf, planetdf, how='left', left_on='planet', 
+                        right_on='planet_name').drop_duplicates()
 
-jwstdf['Rp'] = Rp
-jwstdf['Teq'] = Tp
-
-jwstdf.to_csv(ddir + "jwst_cycle1_planet_prop.csv")
+final_jwstdf.to_csv(ddir + "jwst_cycle1_planet_prop_tess.csv")
 
 #%%
-#Read in TESS List
-tess_file = ddir + "tess_cycle1_planets.txt"
+params = ['Rp', 'Tp', 'Mp','Teff', 'Kmag', 'orbital_period', 'Rs','a/Rs', 'catalog_name', 'planet_name']
 
-tess_list = p.read_csv(tess_file, comment="#", names=['planet'])
+prevdf  = pd.DataFrame(columns = params)
 
-tessdf = p.merge(tess_list, jwstdf, how='inner', right_on="planet", left_on="planet")
-
-#%%
-#Print out cycle 1 transiting planet properites
-Rp =[]
-Tp = []
 for i,planet in unique_prev.iterrows():
      
     try:
-        radii, temp = get_planet_properties(planet['plntname'])
-        print(radii, temp)
-    except:
-        radii = -1
-        temp = -1
+        prevdict = get_planet_properties(planet['plntname'])
+        shortdf = pd.DataFrame([prevdict], columns = params)
+        print(shortdf)
         
-    Rp.append(radii)
-    Tp.append(temp)
+    except:
+        shortdf = pd.DataFrame([{'planet_name' : planet['plntname'], 'catalog_name' : 'error'}])
+        
+        
+    prevdf = pd.concat([prevdf, shortdf])
+    
+final_uniqueprev = pd.merge(unique_prev, prevdf, how='left', 
+                        left_on='plntname', right_on='planet_name').drop_duplicates()
+final_uniqueprev.to_csv(ddir + "transitspec_planet_prop_tess.csv")
 
-unique_prev['Rp'] = Rp
-unique_prev['Teq'] = Tp
-
-unique_prev.to_csv(ddir + "transitspec_planet_prop.csv")
 
 #%%
+final_uniqueprev = pd.read_csv(ddir+"transitspec_planet_prop_tess.csv")
+final_jwstdf = pd.read_csv(ddir + "jwst_cycle1_planet_prop_tess.csv")
+
+final_jwstdf['a/Rs'].iloc[47] = 11.7
+final_jwstdf['Rs'].iloc[47] = 1.38
+final_jwstdf['Teff'].iloc[47] = 5675
+
+#%%
+
+
+def flux_fromTp(Tp):
+    boltzm = 5.670374419e-8  #Wm-2K-4
+    try:
+        I = 4.0 * boltzm * Tp**4
+        return I
+   
+    except TypeError:
+        return -1
+
+#Add in instellation flux
+
+Io = list( map( lambda x : flux_fromTp(x), final_jwstdf['Tp']))
+final_jwstdf['Io'] = Io
+
+Io = list( map( lambda x : flux_fromTp(x), final_uniqueprev['Tp']))
+final_uniqueprev['Io'] = Io
+#%%
+def get_incidentFlux(aRstar,Teff):
+    
+    F = (aRstar*.0046505)**(-2) * Teff**4 / (5778)**4
+    
+    return F
+
+inFlux = list(map (lambda x,y: get_incidentFlux(x,y), final_jwstdf['a/Rs'], final_jwstdf['Teff']))
+final_jwstdf['inFlux'] = inFlux
+
+inFlux = list( map( lambda x,y : get_incidentFlux(x,y), final_uniqueprev['a/Rs'], final_uniqueprev['Teff']))
+final_uniqueprev['inFlux'] = inFlux
+
+
+#%%
+#Here is where we do the plotting.
+#Rp vs Teq 
+
 inst = ['Wide Field Camera 3', 
         'Infared Array Camera (IRAC)', 
         'Space Telescope Imaging Spectrograph',
         'WFC3', 'IRAC', 'WFC',
         'Near Infrared Camera and Multi-Object Spectrometer'
         ]
-aprev = unique_prev.copy()
+
+jwstdf = final_jwstdf
+aprev = final_uniqueprev.copy()
 cond = list(map(lambda x : x in inst, aprev['instrument']))
 prev = aprev[cond]
+
+#%%
+
+
 go1_transit = jwstdf[(jwstdf['calltype']=="GO") & (jwstdf['obstype'] == "transit")]
 #go1_direct = jwstdf[(jwstdf['calltype']=="GO") & (jwstdf['obstype'] == "direct")]
 gto_transit = jwstdf[(jwstdf['calltype']=="GTO") & (jwstdf['obstype'] == "transit")]
@@ -149,24 +188,19 @@ plt.style.use('dark_background')
 plt.figure(figsize=(8,8))
 fnt=15
 
-
-
-plt.plot(go1_transit['Teq'], go1_transit['Rp']*Mj, 's', color="darkorange",
-             fillstyle='full', label="JWST GO1 Transit", ms=10, alpha=0.8)
-#plt.plot(go1_direct['Teq'], go1_direct['Rp']*Mj, 's', color="darkorange",
-#             fillstyle='none', label="JWST GO1 Direct", ms=10)
-
-plt.plot(gto_transit['Teq'], gto_transit['Rp']*Mj, 'ro', fillstyle='full', 
-         label="JWST GTO Transit", ms=8, alpha=0.7)
-#plt.plot(gto_direct['Teq'], gto_direct['Rp']*Mj, 'ro', fillstyle='none', 
-#         label="JWST GTO Direct", ms=8)
-plt.plot(tessdf['Teq'], tessdf['Rp']*Mj, 's', color="white",
-             fillstyle='none', label="TESS Discoveries", ms=10, alpha=1)
-
-plt.plot(prev['Teq'], prev['Rp']*Mj,'w.', ms=5, label="HST/Spitzer < 2021")
+plt.plot(prev['Tp'], prev['Rp']*Mj,'w.', ms=5, label="HST/Spitzer <2021")
 plt.yscale('log', base=10)
 plt.xscale('log', base=10)
 
+
+plt.plot(gto_transit['Tp'], gto_transit['Rp']*Mj, 'r^', fillstyle='full', 
+         label="JWST GTO Transit", ms=9, alpha=0.7)
+#plt.plot(gto_direct['Teq'], gto_direct['Rp']*Mj, 'ro', fillstyle='none', 
+#         label="JWST GTO Direct", ms=8)
+plt.plot(go1_transit['Tp'], go1_transit['Rp']*Mj, 'h', color="darkorange",
+             fillstyle='full', label="JWST GO1 Transit", ms=8, alpha=0.9)
+#plt.plot(go1_direct['Teq'], go1_direct['Rp']*Mj, 's', color="darkorange",
+#             fillstyle='none', label="JWST GO1 Direct", ms=10)
 
 plt.legend(fontsize=fnt-3)
 xrange = [4000,51]
@@ -193,3 +227,247 @@ plt.plot(110, 11.2, 'o', color='khaki', alpha=1, ms=12)
 plt.annotate('Jupiter', (102,11.1), color='khaki', fontsize=fnt)
 
 plt.savefig(ddir+"cycle1_planets_tess.png")
+
+
+#%%
+#Here is where we do the plotting.
+# Mark TESS Planets
+
+def is_tess(planet):
+    tessplanets = ['HD 15337 c', 'L168-9 c', 'L98-59 c', 'LTT 1445 A b', 'TOI 260.01', 
+                   'TOI 776 b', 'HD 15337 b',
+                   'TOI 776 c', 'TOI 836.01', 'TOI 836.02', 'LTT 1445 A b',
+                   'TOI 178 b', 'TOI 178 d','TOI 178 g','TOI 421 b','TOI 421 b',
+                   'LHS 3844 b', 'WD 1856+534 b','HIP 67522 b', 'GJ 486 b',
+                   'TOI 741.01', 'GJ 357 b', 'TOI 910.01',
+                   'GJ 367 b', 'L 98-59 d', 'LP 791-18 c', 
+                   'LTT 9779 b']
+    
+    if planet in tessplanets:
+        return True
+    else: 
+        return False
+
+tesspl = list( map( lambda x : is_tess(x), jwstdf['planet_name']))
+jwstdf = final_jwstdf
+jwstdf['tess'] = tesspl
+
+
+
+#%%
+
+go1_transit = jwstdf[(jwstdf['calltype']=="GO") & (jwstdf['obstype'] == "transit")]
+
+gto_transit = jwstdf[(jwstdf['calltype']=="GTO") & (jwstdf['obstype'] == "transit")]
+
+tess_transit = jwstdf[(jwstdf['tess']) & (jwstdf['obstype'] == 'transit')]
+
+go1 = jwstdf[(jwstdf['calltype']=="GO")]
+gto = jwstdf[(jwstdf['calltype']=="GTO")]
+
+Mj = 11.2089
+#Create plots
+plt.style.use('dark_background')
+
+plt.figure(figsize=(8,8))
+fnt=15
+
+plt.plot(gto_transit['Tp'], gto_transit['Rp']*Mj, 'r^', fillstyle='full', 
+         label="JWST GTO Transit", ms=9, alpha=0.7)
+
+plt.plot(go1_transit['Tp'], go1_transit['Rp']*Mj, 'h', color="darkorange",
+             fillstyle='full', label="JWST GO1 Transit", ms=8, alpha=0.9)
+
+plt.plot(tess_transit['Tp'], tess_transit['Rp']*Mj, 'D', color="white", 
+             fillstyle='none', ms=10, label="TESS Planet")
+
+plt.yscale('log', base=10)
+plt.xscale('log', base=10)
+plt.legend(fontsize=fnt-3)
+xrange = [4000,51]
+plt.xlim(xrange[0], xrange[1])
+plt.ylim(.6, 31)
+plt.xlabel('Equilibrium Temperature (K)', fontsize=fnt)
+plt.ylabel('Planet Radius (Earth)', fontsize=fnt)
+xpos = [1000, 300,100]
+xlab = ["1000","300", "100"]
+plt.xticks(xpos, xlab, fontsize=fnt)
+ypos = [1,3,10,30]
+ylab = ["1", "3", "10", "30"]
+plt.yticks(ypos,ylab, fontsize=fnt)
+
+
+plt.title("JWST Cycle 1 Transiting Exoplanets", fontsize=fnt)
+
+#annotate
+
+plt.plot(219, 1, 'o', color='cyan', alpha=1, ms=12)
+plt.annotate('Earth', (205,.96), color='cyan', fontsize=fnt)
+
+plt.plot(110, 11.2, 'o', color='khaki', alpha=1, ms=12)
+plt.annotate('Jupiter', (102,11.1), color='khaki', fontsize=fnt)
+
+plt.savefig(ddir+"cycle1_planets_tesspl.png")
+
+#%%
+#Teq vs Radius and stellar temperature marking the TESS planets.
+
+transit = jwstdf[ (jwstdf['obstype'] == "transit")]
+
+tess_transit = jwstdf[(jwstdf['tess']) & (jwstdf['obstype'] == 'transit')]
+
+#go1 = jwstdf[(jwstdf['calltype']=="GO")]
+#gto = jwstdf[(jwstdf['calltype']=="GTO")]
+
+Mj = 11.2089
+
+#Create plots
+plt.style.use('dark_background')
+
+plt.figure(figsize=(9.3,8))
+fnt=15
+
+
+plt.scatter(transit['Tp'], transit['Rp']*Mj, marker='o', c=transit['Teff'], cmap='RdYlBu',
+              label="JWST Transit", alpha=1, s=45)
+plt.colorbar(label='Stellar Teff (K)')
+
+plt.plot(tess_transit['Tp'], tess_transit['Rp']*Mj, 'D', color="white", 
+             fillstyle='none', ms=10, label="TESS Planet")
+
+plt.yscale('log', base=10)
+plt.xscale('log', base=10)
+plt.legend(fontsize=fnt-3)
+xrange = [4000,51]
+plt.xlim(xrange[0], xrange[1])
+plt.ylim(.6, 31)
+plt.xlabel('Equilibrium Temperature (K)', fontsize=fnt)
+plt.ylabel('Planet Radius (Earth)', fontsize=fnt)
+xpos = [1000, 300,100]
+xlab = ["1000","300", "100"]
+plt.xticks(xpos, xlab, fontsize=fnt)
+ypos = [1,3,10,30]
+ylab = ["1", "3", "10", "30"]
+plt.yticks(ypos,ylab, fontsize=fnt)
+
+
+plt.title("JWST Cycle 1 Transiting Exoplanets", fontsize=fnt)
+
+#annotate
+
+plt.plot(219, 1, 'o', color='cyan', alpha=1, ms=12)
+plt.annotate('Earth', (205,.96), color='cyan', fontsize=fnt)
+
+plt.plot(110, 11.2, 'o', color='khaki', alpha=1, ms=12)
+plt.annotate('Jupiter', (102,11.1), color='khaki', fontsize=fnt)
+
+plt.savefig(ddir+"cycle1_planets_tess_star.png")
+
+
+#%%
+#In Incident Flux and TESS planets marked.
+
+transit = jwstdf[ (jwstdf['obstype'] == "transit")]
+
+tess_transit = jwstdf[(jwstdf['tess']) & (jwstdf['obstype'] == 'transit')]
+
+#go1 = jwstdf[(jwstdf['calltype']=="GO")]
+#gto = jwstdf[(jwstdf['calltype']=="GTO")]
+
+Mj = 11.2089
+
+#Create plots
+plt.style.use('dark_background')
+
+plt.figure(figsize=(11,7))
+fnt=15
+
+
+plt.scatter(transit['inFlux'], transit['Rp']*Mj, marker='o', c=transit['Teff'], cmap='RdYlBu',
+              label="JWST Transit", alpha=1, s=45)
+plt.colorbar(label='Stellar Teff (K)')
+
+plt.plot(tess_transit['inFlux'], tess_transit['Rp']*Mj, 'D', color="white", 
+             fillstyle='none', ms=10, label="TESS Planet")
+
+plt.yscale('log', base=10)
+plt.xscale('log', base=10)
+plt.legend(fontsize=fnt-3)
+xrange = [10000,.01]
+plt.xlim(xrange[0], xrange[1])
+plt.ylim(.6, 31)
+plt.xlabel('Incident Flux (Earth)', fontsize=fnt)
+plt.ylabel('Planet Radius (Earth)', fontsize=fnt)
+xpos = [1000, 300,100, 30, 1, .3, .1]
+xlab = ["1000","300", "100","30", "1", "0.3", ".1"]
+plt.xticks(xpos, xlab, fontsize=fnt)
+ypos = [1,3,10,30]
+ylab = ["1", "3", "10", "30"]
+plt.yticks(ypos,ylab, fontsize=fnt)
+
+
+plt.title("JWST Cycle 1 Transiting Exoplanets", fontsize=fnt)
+
+#annotate
+
+plt.plot(1, 1, 'o', color='cyan', alpha=1, ms=12)
+plt.annotate('Earth', (1.1,0.83), color='cyan', fontsize=fnt)
+
+plt.plot(.037, 11.2, 'o', color='khaki', alpha=1, ms=12)
+plt.annotate('Jupiter', (.05,9.0), color='khaki', fontsize=fnt)
+
+plt.savefig(ddir+"cycle1_planets_tess_influx.png")
+
+#%%
+#In Incident Flux and wihtout TESS planets
+
+transit = jwstdf[ (jwstdf['obstype'] == "transit")]
+
+tess_transit = jwstdf[(jwstdf['tess']) & (jwstdf['obstype'] == 'transit')]
+
+#go1 = jwstdf[(jwstdf['calltype']=="GO")]
+#gto = jwstdf[(jwstdf['calltype']=="GTO")]
+
+Mj = 11.2089
+
+#Create plots
+plt.style.use('dark_background')
+
+plt.figure(figsize=(11,7))
+fnt=15
+
+
+plt.scatter(transit['inFlux'], transit['Rp']*Mj, marker='o', c=transit['Teff'], cmap='RdYlBu',
+              label="JWST Transit", alpha=1, s=45)
+plt.colorbar(label='Stellar Teff (K)')
+
+#plt.plot(tess_transit['inFlux'], tess_transit['Rp']*Mj, 'D', color="white", 
+#             fillstyle='none', ms=10, label="TESS Planet")
+
+plt.yscale('log', base=10)
+plt.xscale('log', base=10)
+plt.legend(fontsize=fnt-3)
+xrange = [10000,.01]
+plt.xlim(xrange[0], xrange[1])
+plt.ylim(.6, 31)
+plt.xlabel('Incident Flux (Earth)', fontsize=fnt)
+plt.ylabel('Planet Radius (Earth)', fontsize=fnt)
+xpos = [1000, 300,100, 30, 1, .3, .1]
+xlab = ["1000","300", "100","30", "1", "0.3", ".1"]
+plt.xticks(xpos, xlab, fontsize=fnt)
+ypos = [1,3,10,30]
+ylab = ["1", "3", "10", "30"]
+plt.yticks(ypos,ylab, fontsize=fnt)
+
+
+plt.title("JWST Cycle 1 Transiting Exoplanets", fontsize=fnt)
+
+#annotate
+
+plt.plot(1, 1, 'o', color='cyan', alpha=1, ms=12)
+plt.annotate('Earth', (1.1,0.83), color='cyan', fontsize=fnt)
+
+plt.plot(.037, 11.2, 'o', color='khaki', alpha=1, ms=12)
+plt.annotate('Jupiter', (.05,9.0), color='khaki', fontsize=fnt)
+
+plt.savefig(ddir+"cycle1_planets_influx.png")
